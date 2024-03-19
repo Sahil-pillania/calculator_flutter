8d89edf2346f0629d03733b514c00ad4249e3f65
Sahil pillania <sahilpillania88@gmail.com>
2024-03-19 20:52:15 +0530

    icecream view created

---
+++ lib/icecream/views/icecream.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+
+class Icecream_view extends StatefulWidget {
+  const Icecream_view({super.key});
+
+  @override
+  State<Icecream_view> createState() => _Icecream_viewState();
+}
+
+class _Icecream_viewState extends State<Icecream_view> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
