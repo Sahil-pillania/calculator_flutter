8ae7700ea1067eb772505b0d96dd6ae6a83867c8
Sahil pillania <sahilpillania88@gmail.com>
2024-03-21 21:54:46 +0530

    added method

--- lib/icecream/views/icecream.dart
+++ lib/icecream/views/icecream.dart
@@ -1,5 +1,6 @@
 import "dart:convert";
 
+import "package:calculatorapp/icecream/model/icecream.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
@@ -13,12 +14,17 @@ class Icecream_view extends StatelessWidget {
   //   super.initState();
   // }
 
-  Future<Map<String, dynamic>> loadIcecreams() async {
+  Future<List<Icecream>?> loadIcecreams() async {
     final rawIcecream = await rootBundle.loadString("assets/icecream.json");
-    final decodedIcecreams = jsonDecode(rawIcecream);
+    // final decodedIcecreams = jsonDecode(rawIcecream);
     // print(decodedIcecreams);
-    await Future.delayed(const Duration(seconds: 3));
-    return decodedIcecreams;
+    final icecreams = icecreamDatafromJson(rawIcecream);
+    print("debugging ************************************************");
+    print("Raw ice cream data: $rawIcecream");
+    print("Parsed ice cream data: $icecreams");
+    print("Ice creams list: ${icecreams?.icecreams}");
+    await Future.delayed(const Duration(seconds: 1));
+    return icecreams!.icecreams;
   }
 
   @override
@@ -26,7 +32,7 @@ class Icecream_view extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(
+        const Text(
           "Icecreams",
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
@@ -44,9 +50,10 @@ class Icecream_view extends StatelessWidget {
                   future: loadIcecreams(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.done) {
-                      final icecreams = snapshot.data as Map<String, dynamic>;
+                      final icecreams = snapshot.data;
 
-                      return Text(icecreams["icecreams"][0]["flavor"]);
+                      return Text(icecreams!.first.flavor);
+                      // return Text("data");
                     } else {
                       return const Center(
                           child: CircularProgressIndicator.adaptive());
